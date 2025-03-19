295431ce4d3b6ded89ea9c02653307b53ce44916
Nayakanti Pavan Kumar <nayakantipavankumar@gmail.com>
2025-03-19 21:12:19 +0530

    made id as primary key

--- BloodBank/register/models.py
+++ BloodBank/register/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UserRegister(models.Model):
-    Id=models.AutoField()
+    Id=models.AutoField(primary_key=True)
     Name= models.CharField(max_length=100)
     BloodGroup=models.CharField(max_length=3)
     City=models.CharField(max_length=30)
