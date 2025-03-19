c1a935a43fdf2922109f304c2741259037c9cc2b
Nayakanti Pavan Kumar <nayakantipavankumar@gmail.com>
2025-03-19 21:09:02 +0530

    created a model for userregistration

--- BloodBank/register/models.py
+++ BloodBank/register/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserRegister(models.Model):
+    Id=models.AutoField()
+    Name= models.CharField(max_length=100)
+    BloodGroup=models.CharField(max_length=3)
+    City=models.CharField(max_length=30)
+    State=models.CharField(max_length=50)
+    Age=models.IntegerField()
