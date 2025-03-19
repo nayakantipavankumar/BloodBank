66158c4bc7bf54e233ee5ab256f8e66cb86b2f43
Nayakanti Pavan Kumar <nayakantipavankumar@gmail.com>
2025-03-19 21:47:04 +0530

    added basic view

--- BloodBank/register/views.py
+++ BloodBank/register/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import UserRegister
 
 # Create your views here.
+
+def UserRegisterView(request):
+    Data=request.POST
+    print(Data)
+    return render("submitted.html")
